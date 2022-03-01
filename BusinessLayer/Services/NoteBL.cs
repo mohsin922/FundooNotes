6182d2c78feff91afabd5958a10c11ae30ffb5e2
peermohsin22 <peermohsin22@gmail.com>
2022-03-01 23:53:12 +0530

    Created Interfaces and services for Crud Operations

---
+++ BusinessLayer/Services/NoteBL.cs
@@ -0,0 +1,47 @@
+﻿using BusinessLayer.Interfaces;
+using CommonLayer.Models;
+using RepositoryLayer.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BusinessLayer.Services
+{
+    public class NoteBL : INoteBL
+    {
+        private readonly INoteBL noteBL;
+        public NoteBL(INoteBL noteBL)
+        {
+            this.noteBL = noteBL;
+        }
+
+        /// <summary>
+        /// Adding a new Note Method
+        /// </summary>
+        /// <param name="noteModel"></param>
+        /// <returns></returns>
+        public bool CreateNote(NoteModel noteModel)
+        {
+            try
+            {
+                return this.noteBL.CreateNote(noteModel);
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+
+        public IEnumerable<Note> GetAllNotes()
+        {
+            try
+            {
+                return this.noteBL.GetAllNotes();
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+    }
+}
---
+++ RepositoryLayer/Services/NoteRL.cs
@@ -0,0 +1,78 @@
+﻿using CommonLayer.Models;
+using Microsoft.Extensions.Configuration;
+using RepositoryLayer.Context;
+using RepositoryLayer.Entities;
+using RepositoryLayer.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace RepositoryLayer.Services
+{
+    public class NoteRL : INoteRL
+    {
+        public readonly FundooContext fundooContext;
+        IConfiguration _configure;
+
+        public NoteRL(FundooContext fundooContext, IConfiguration configure)
+        {
+            this.fundooContext = fundooContext;
+            _configure = configure;
+        }
+
+        /// <summary>
+        /// Creating a CreateNote Method
+        /// </summary>
+        /// <param name="noteModel"></param>
+        public bool CreateNote(NoteModel noteModel)
+        {
+            try
+            {
+                Note newNotes = new Note();
+                newNotes.NotedId = noteModel.NotedId;
+                newNotes.Title = noteModel.Title;
+                newNotes.NoteBody = noteModel.NoteBody;
+                newNotes.Reminder = noteModel.Reminder;
+                newNotes.Color = noteModel.Color;
+                newNotes.BImage = noteModel.BImage;
+                newNotes.IsArchived = noteModel.IsArchived;
+                newNotes.IsPinned = noteModel.IsPinned;
+                newNotes.IsDeleted = noteModel.IsDeleted;
+                newNotes.CreatedAt = DateTime.Now;
+                newNotes.ModifiedAt = DateTime.Now;
+                
+                this.fundooContext.NotesTable.Add(newNotes); //Adding  data to database
+                //Save the changes in database
+                int result = this.fundooContext.SaveChanges();
+                if (result > 0)
+                {
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+
+        /// <summary>
+        /// Show all his notes to user
+        /// </summary>
+        public IEnumerable<Note> GetAllNotes()
+        {
+            try
+            {
+                return this.fundooContext.NotesTable.ToList();
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+    }
+}
