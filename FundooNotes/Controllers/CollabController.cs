ae8e88bf428a5d5b3f16d586fbc8313c499caa79
peermohsin22 <peermohsin22@gmail.com>
2022-03-07 19:55:34 +0530

    Redis for Displaying all Notes and Fixed Some Style Cop Warnings

--- FundooNotes/Controllers/CollabController.cs
+++ FundooNotes/Controllers/CollabController.cs
@@ -1,99 +1,103 @@
-﻿using BusinessLayer.Interfaces;
-using CommonLayer.Models;
-using Microsoft.AspNetCore.Authorization;
-using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Mvc;
-using RepositoryLayer.Context;
-using RepositoryLayer.entities;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-
-namespace FundooNotes.Controllers
+﻿namespace Microsoft.Xxx
 {
-    [Route("api/[controller]")]
-    [ApiController]
-    [Authorize]
-    public class CollabController : ControllerBase
+    using System;
+    using BusinessLayer.Interfaces;
+    using CommonLayer.Models;
+    using Microsoft.AspNetCore.Authorization;
+    using Microsoft.AspNetCore.Http;
+    using Microsoft.AspNetCore.Mvc;
+    using RepositoryLayer.Context;
+    using RepositoryLayer.entities;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Threading.Tasks;
+
+
+    namespace FundooNotes.Controllers
     {
-        private readonly ICollabBL collabBL;
-        private readonly FundooContext fundooContext;
-        public CollabController(ICollabBL collabBL, FundooContext fundooContext)
+        [Route("api/[controller]")]
+        [ApiController]
+        [Authorize]
+        public class CollabController : ControllerBase
         {
-            this.collabBL = collabBL;
-            this.fundooContext = fundooContext;
-        }
-
-        [HttpPost]
-        [Route("Add")]
-        public IActionResult AddCollaboration(CollabModel collabModel)
-        {
-            try
+            private readonly ICollabBL collabBL;
+            private readonly FundooContext fundooContext;
+            public CollabController(ICollabBL collabBL, FundooContext fundooContext)
             {
-                long userId = Convert.ToInt32(User.Claims.FirstOrDefault(e => e.Type == "Id").Value);
-                var collab = this.fundooContext.NotesTable.Where(X => X.NotesId == collabModel.NotesId).SingleOrDefault();
-                if (collab.Id == userId)
+                this.collabBL = collabBL;
+                this.fundooContext = fundooContext;
+            }
+
+            [HttpPost]
+            [Route("Add")]
+            public IActionResult AddCollaboration(CollabModel collabModel)
+            {
+                try
                 {
-                    var result = this.collabBL.AddCollaboration(collabModel);
-                    if (result)
+                    long userId = Convert.ToInt32(User.Claims.FirstOrDefault(e => e.Type == "Id").Value);
+                    var collab = this.fundooContext.NotesTable.Where(X => X.NotesId == collabModel.NotesId).SingleOrDefault();
+                    if (collab.Id == userId)
                     {
-                        return this.Ok(new { status = 200, isSuccess = true, message = "Collaborated successfully!", data = result });
+                        var result = this.collabBL.AddCollaboration(collabModel);
+                        if (result)
+                        {
+                            return this.Ok(new { status = 200, isSuccess = true, message = "Collaborated successfully!", data = result });
+                        }
+                        return this.BadRequest(new { status = 400, isSucess = false, message = "Failed to collaborate!" });
                     }
-                    return this.BadRequest(new { status = 400, isSucess = false, message = "Failed to collaborate!" });
+                    else
+                    {
+                        return this.Unauthorized(new { status = 401, isSucess = false, Message = "Not Authorized to Add Collaboration!" });
+                    }
                 }
-                else
+                catch (Exception e)
                 {
-                    return this.Unauthorized(new { status = 401, isSucess = false, Message = "Not Authorized to Add Collaboration!" });
+                    return this.BadRequest(new { Status = 401, isSuccess = false, Message = e.Message, InnerException = e.InnerException });
                 }
-            }
-            catch (Exception e)
-            {
-                return this.BadRequest(new { Status = 401, isSuccess = false, Message = e.Message, InnerException = e.InnerException });
+
             }
 
-        }
-
-        [HttpGet("Get")]
-        public IActionResult GetAllCollabs(long NotesId)
-        {
-            try
+            [HttpGet("Get")]
+            public IActionResult GetAllCollabs(long NotesId)
             {
-                long userId = Convert.ToInt32(User.Claims.FirstOrDefault(e => e.Type == "Id").Value);
-                var result = collabBL.GetAllCollabs(NotesId);
-                if (result != null)
+                try
                 {
-                    return this.Ok(new { isSuccess = true, message = " All Collaborators found Successfully", data = result });
+                    long userId = Convert.ToInt32(User.Claims.FirstOrDefault(e => e.Type == "Id").Value);
+                    var result = collabBL.GetAllCollabs(NotesId);
+                    if (result != null)
+                    {
+                        return this.Ok(new { isSuccess = true, message = " All Collaborators found Successfully", data = result });
 
+                    }
+                    else
+                    {
+                        return this.NotFound(new { isSuccess = false, message = "No Collaborator  Found" });
+                    }
                 }
-                else
+
+                catch (Exception ex)
                 {
-                    return this.NotFound(new { isSuccess = false, message = "No Collaborator  Found" });
+                    return this.BadRequest(new { Status = 401, isSuccess = false, message = ex.InnerException.Message });
                 }
             }
 
-            catch (Exception ex)
+            [HttpDelete("Remove")]
+            public IActionResult RemoveCollab(long collabID)
             {
-                return this.BadRequest(new { Status = 401, isSuccess = false, message = ex.InnerException.Message });
-            }
-        }
-
-        [HttpDelete("Remove")]
-        public IActionResult RemoveCollab(long collabID)
-        {
-            try
-            {
-                long userid = Convert.ToInt32(User.Claims.FirstOrDefault(e => e.Type == "Id").Value);
-                var result = this.collabBL.RemoveCollab(collabID);
-                if (result != null)
+                try
                 {
-                    return this.Ok(new { status = 200, isSuccess = true, Message = "Removed collab for User sucessfully!", data = collabID });
+                    long userid = Convert.ToInt32(User.Claims.FirstOrDefault(e => e.Type == "Id").Value);
+                    var result = this.collabBL.RemoveCollab(collabID);
+                    if (result != null)
+                    {
+                        return this.Ok(new { status = 200, isSuccess = true, Message = "Removed collab for User sucessfully!", data = collabID });
+                    }
+                    return this.BadRequest(new { status = 400, isSuccess = false, Message = "Sorry! Couldn't remove the User from Collaboration. " });
                 }
-                return this.BadRequest(new { status = 400, isSuccess = false, Message = "Sorry! Couldn't remove the User from Collaboration. " });
-            }
-            catch (Exception e)
-            {
-                return this.BadRequest(new { status = 400, isSuccess = false, Message = e.InnerException.Message });
+                catch (Exception e)
+                {
+                    return this.BadRequest(new { status = 400, isSuccess = false, Message = e.InnerException.Message });
+                }
             }
         }
     }
--- FundooNotes/Startup.cs
+++ FundooNotes/Startup.cs
@@ -94,6 +94,12 @@ namespace FundooNotes
             services.AddTransient<ICollabRL, CollabRL>();
             services.AddTransient<ILabelBL, LabelBL>();
             services.AddTransient<ILabelRL, LabelRL>();
+
+            services.AddStackExchangeRedisCache(options =>
+            {
+                options.Configuration = "localhost:6379";
+            });
+            services.AddMemoryCache();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
